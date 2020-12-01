76a6858df6cb59fba2e8b3e1d3074201b637d446
juniordugue <jrdugue@gmail.com>
2020-12-01 14:12:09 -0500

    fixed the bug and added useeffect and usestate to access additional characters from the api and have their names appear correctly, created loadmore button to load additional characters

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const defaultEndpoint = "https://rickandmortyapi.com/api/character/";
 
@@ -16,7 +17,48 @@ export async function getServerSideProps() {
 
 export default function Home({ data }) {
   // console.log("data", data);
-  const { results = [] } = data;
+  const { info, results: defaultResults = [] } = data;
+  const [results, setResults] = useState(defaultResults);
+  const [page, setPage] = useState({
+    ...info,
+    current: defaultEndpoint,
+  });
+
+  const { current } = page;
+
+  useEffect(() => {
+    if (current === defaultEndpoint) return;
+
+    async function request() {
+      const res = await fetch(current);
+      const nextData = await res.json();
+
+      setPage({
+        current,
+        ...nextData.info,
+      });
+
+      if (!nextData.info?.prev) {
+        setResults(nextData.results);
+        return;
+      }
+
+      setResults((prev) => {
+        return [...prev, ...nextData.results];
+      });
+    }
+    request();
+  }, [current]);
+
+  function handleLoadMore(){
+    setPage(prev => {
+      return {
+        ...prev, 
+        current: page?.next
+      }
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -25,9 +67,11 @@ export default function Home({ data }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Wubba Lubba Dub Dub!</h1>
+        {/* <h1 className={styles.title}>Wubba Lubba Dub Dub!</h1> */}
+        <h1 className={styles.title}>Rick and Morty Wiki</h1>
 
-        <p className={styles.description}>Rick and Morty Wiki</p>
+        {/* <p className={styles.description}>Rick and Morty Wiki</p> */}
+        <p className={styles.description}>Wubba Lubba Dub Dub!</p>
 
         <ul className={styles.grid}>
           {results.map((results) => {
@@ -37,14 +81,16 @@ export default function Home({ data }) {
               <li key={id} className={styles.card}>
                 <a href="https://nextjs.org/docs">
                   {/* <Image src={image} width={200} height={200} alt={`${name} thumbnail`}/> */}
-                  <img src={image} alt={`${name} thumbnail`}/>
-                  <h3>{ name }</h3>
+                  <img src={image} alt={`${name} thumbnail`} />
+                  <h3>{name}</h3>
                 </a>
               </li>
             );
           })}
-
         </ul>
+        <p>
+          <button onClick={handleLoadMore}>Load More</button>
+        </p>
       </main>
 
       <footer className={styles.footer}>
